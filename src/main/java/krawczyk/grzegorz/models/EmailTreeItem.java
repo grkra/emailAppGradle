3f633b1002b23223ba6d5042e03446819ed5dd06
Grzegorz Krawczyk <grzegorz.krawczyk.programista@gmail.com>
2025-11-22 21:53:26 +0100

    new messages appear in Email Table List

--- src/main/java/krawczyk/grzegorz/models/EmailTreeItem.java
+++ src/main/java/krawczyk/grzegorz/models/EmailTreeItem.java
@@ -37,14 +37,38 @@ public class EmailTreeItem<String> extends TreeItem<String> {
     }
 
     /**
-     * Method creates email message in the app and adds it to the list of messages in current folder.
+     * Method creates email message in the application and adds it to the TOP of the list of messages in current folder.
      * <hr></hr>
-     * This method gets and object of the class Email (which is an email message on the email server provider side)
+     * This method gets and object of the class Message (which is an email message on the email server provider side)
      * and transforms it to object of the class EmailMessage (which is and email message inside the app)
      * @param message - object of the class Message (email on email account provider side)
      * @throws MessagingException
      */
     public void addEmail(Message message) throws MessagingException {
+        EmailMessage emailMessage = fetchMessage(message);
+        this.emailMessages.add(emailMessage);
+    }
+
+    /**
+     * Method creates email message in the application and adds it to the END of the list of messages in current folder.
+     * <hr></hr>
+     * This method gets and object of the class Message (which is an email message on the email server provider side)
+     * and transforms it to object of the class EmailMessage (which is and email message inside the app)
+     * @param message - object of the class Message (email on email account provider side)
+     * @throws MessagingException
+     */
+    public void addEmailToTop(Message message) throws MessagingException {
+        EmailMessage emailMessage = fetchMessage(message);
+        this.emailMessages.add(0,emailMessage);
+    }
+
+    /**
+     * Method creates new EmailMessage object (which is and email message inside the app)
+     * from Message object (which is an email message on the email server provider side)
+     * @param message - object of the class Message (email on email account provider side)
+     * @throws MessagingException
+     */
+    private EmailMessage fetchMessage(Message message) throws MessagingException {
         boolean messageWasRead = message.getFlags().contains(Flags.Flag.SEEN);
         EmailMessage emailMessage = new EmailMessage(
                 message.getSubject(),
@@ -56,11 +80,11 @@ public class EmailTreeItem<String> extends TreeItem<String> {
                 message
         );
 
-        emailMessages.add(emailMessage);
         if (!messageWasRead) {
             incrementMessageCount();
         }
-        System.out.println("Added to " + name + " " + message.getSubject());
+
+        return emailMessage;
     }
 
     public void incrementMessageCount() {
