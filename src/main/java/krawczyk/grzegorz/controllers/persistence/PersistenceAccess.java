fb13f0a882da1078e5c6aa3820c30f14a35feb4c
Grzegorz Krawczyk <grzegorz.krawczyk.programista@gmail.com>
2025-12-15 22:09:43 +0100

    added encoding and decoding password when saving logged email account to local file

--- src/main/java/krawczyk/grzegorz/controllers/persistence/PersistenceAccess.java
+++ src/main/java/krawczyk/grzegorz/controllers/persistence/PersistenceAccess.java
@@ -15,6 +15,9 @@ public class PersistenceAccess {
      */
     private String VALID_ACCOUNTS_LOCATION = System.getProperty("user.home") + File.separator + "validAccounts.ser";
 
+    private Encoder encoder = new Encoder();
+
+
     /**
      * Method reads list of valid accounts from local file.
      * Method is called the earliest possible - in the Launcher.start() method
@@ -26,6 +29,7 @@ public class PersistenceAccess {
             FileInputStream fileInputStream = new FileInputStream(this.VALID_ACCOUNTS_LOCATION);
             ObjectInputStream objectInputStream = new ObjectInputStream(fileInputStream);
             List<ValidAccount> persistedList = (List<ValidAccount>) objectInputStream.readObject();
+            this.decodePasswords(persistedList);
             resultList.addAll(persistedList);
         } catch (Exception e) {
             e.printStackTrace();
@@ -45,12 +49,35 @@ public class PersistenceAccess {
             File file = new File(this.VALID_ACCOUNTS_LOCATION);
             FileOutputStream fileOutputStream = new FileOutputStream(file);
             ObjectOutputStream objectOutputStream = new ObjectOutputStream(fileOutputStream);
+            this.encodePasswords(validAccounts);
             objectOutputStream.writeObject(validAccounts);
 
             objectOutputStream.close();
             fileOutputStream.close();
         } catch (Exception e) {
             e.printStackTrace();
+        }
+    }
+
+    /**
+     * Method decodes password read from file and saves decoded password in the ValidAccount
+     * @param persistedList list of ValidAccounts read from persisted file
+     */
+    private void decodePasswords(List<ValidAccount> persistedList) {
+        for (ValidAccount validAccount: persistedList) {
+            String passwordFromFile = validAccount.getPassword();
+            validAccount.setPassword(encoder.decode(passwordFromFile));
+        }
+    }
+
+    /**
+     * Method encodes password to write it to persisted file
+     * @param persistedList list of ValidAccounts to be saved in persisted file
+     */
+    private void encodePasswords(List<ValidAccount> persistedList) {
+        for (ValidAccount validAccount: persistedList) {
+            String passwordToEncode = validAccount.getPassword();
+            validAccount.setPassword(encoder.encode(passwordToEncode));
         }
     }
 }
